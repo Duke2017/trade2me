ddf1a5f9f0c887e25abc8234fb3bdd1cbdf8cfde
Tabakov <bel2017@gmail.com>
2023-07-06 18:23:15 +0300

    fix env again

--- tgbot/config/config.service.ts
+++ tgbot/config/config.service.ts
@@ -11,7 +11,7 @@ export class ConfigService implements IConfigService {
         this.config = parsed;
     }
     get(key:string) : string {
-        const res = this.config[key] || process.env[key] || '';
+        const res = process.env[key] || this.config[key] || '';
         return res;
     }
 }
